c83222d70f7448b304ab52522fe74a673a4a3215
jidemobell <jidd32@gmail.com>
2020-02-11 20:13:57 +0100

    Updates

---
+++ precache-manifest.aa246907e35015f8646435c4a555d404.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d15f7161e48c60f3a2d601991dd675",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5e6c0512ea2087257db",
+    "url": "/static/css/2.3ad756a6.chunk.css"
+  },
+  {
+    "revision": "e40637b2162a04430970",
+    "url": "/static/css/main.25791f35.chunk.css"
+  },
+  {
+    "revision": "f5e6c0512ea2087257db",
+    "url": "/static/js/2.059f5a5d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.059f5a5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e40637b2162a04430970",
+    "url": "/static/js/main.057196b6.chunk.js"
+  },
+  {
+    "revision": "cb56da52a45e1ba8ade8",
+    "url": "/static/js/runtime-main.39d3cbc1.js"
+  },
+  {
+    "revision": "a62b69e5373fa2f72533755927ebd5bd",
+    "url": "/static/media/big_sign.a62b69e5.png"
+  },
+  {
+    "revision": "540c6e07a5832057379d843aed79daa8",
+    "url": "/static/media/cropped-bridals.540c6e07.png"
+  },
+  {
+    "revision": "4b48e739204e22c87e3e503b80c50373",
+    "url": "/static/media/fb.4b48e739.svg"
+  },
+  {
+    "revision": "2295a344ff830939116d39e31a0a2a44",
+    "url": "/static/media/ig2.2295a344.svg"
+  },
+  {
+    "revision": "3b5b92922e0d20f0b6a05ca30137488f",
+    "url": "/static/media/remixicon.3b5b9292.eot"
+  },
+  {
+    "revision": "76414508f73cb3fb57073b235c53fa6c",
+    "url": "/static/media/remixicon.76414508.woff2"
+  },
+  {
+    "revision": "c0d34ad379cbf1557481a30be08263ff",
+    "url": "/static/media/remixicon.c0d34ad3.ttf"
+  },
+  {
+    "revision": "d0ac4bc43acfbe23145c4e5730f48c7f",
+    "url": "/static/media/remixicon.d0ac4bc4.svg"
+  },
+  {
+    "revision": "e5b755b03df25eabc565694ec9aad29f",
+    "url": "/static/media/remixicon.e5b755b0.woff"
+  }
+]);
